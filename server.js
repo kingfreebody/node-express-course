72b6954c66033eb297ab0ff95515f98a8d9c8eb4
King Freebody <King.Freebody@gmail.com>
2020-05-11 22:30:12 +0200

    add first GET route

--- server.js
+++ server.js
@@ -1,6 +1,23 @@
 const express = require('express');
 const app = express();
 
+const mockUserData = [
+    {name: 'Mark'},
+    {name: 'Jill'}
+]
+
+app.get('/users', function(req, res){
+    res.json(
+        {
+            success: true,
+            message: 'successfully got users. Nice!',
+            users: mockUserData
+        }
+    )
+})
+
 app.listen(8000, function(){
-    console.log("server is running")
+    console.log("")
+    console.log("Server is running... press Ctrl-C to stop.")
+    //console.log("If server script is changed, server is automatically restarted by nodemon.")
 })
