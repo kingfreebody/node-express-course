69e33a05b8f820ced0080f6e300e6da9c657061f
King Freebody <King.Freebody@gmail.com>
2020-05-11 22:58:08 +0200

    add dynamic GET route

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const mockUserData = [
 ]
 
 app.get('/users', function(req, res){
+    console.log("what the heck?")
     res.json(
         {
             success: true,
@@ -14,6 +15,15 @@ app.get('/users', function(req, res){
             users: mockUserData
         }
     )
+})
+
+app.get('/users/:id', function(req, res){
+    console.log(req.params.id)
+    res.json({
+        success: true,
+        message: 'got one user',
+        user: req.params.id
+    })
 })
 
 app.listen(8000, function(){
